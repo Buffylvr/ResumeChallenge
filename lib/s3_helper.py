9e9d1b1437396e708964620759fb05dd6a6d96e8
Buffylvr <buffylvr777@hotmail.com>
2024-10-28 20:56:01 -0400

    updating s3 helper with some more stuff

--- lib/s3_helper.py
+++ lib/s3_helper.py
@@ -5,8 +5,6 @@ import boto3
 
 s3 = boto3.client('s3')
 
-
-
 def create_bucket(bucket_name, region):
     response =s3.create_bucket(
         Bucket=bucket_name,
@@ -22,11 +20,34 @@ def create_bucket(bucket_name, region):
 def delete_bucket(bucket_name, bucket_owner):
 	#bucket_owner is expected per https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/s3/client/delete_bucket.html
 	#bucket_owner is the account ID
-	response = s3.delete_bucket(
-		Bucket=bucket_name,
-		ExpectedBucketOwner=bucket_owner
-	)
+    response = s3.delete_bucket(
+	    Bucket=bucket_name,
+	    ExpectedBucketOwner=bucket_owner
+    )
     print(response)
 
+
+#this function only gives you the first name of the first item back
+def list_bucket_contents(bucket_name):
+    response = s3.list_objects(
+        Bucket=bucket_name
+    )
+    #print(response)
+    item_name = response['Contents'][0]['Key']
+    print(item_name)
+
+
+def list_all_bucket_contents(bucket_name):
+    response = s3.list_objects(
+        Bucket=bucket_name
+    )
+#    print("printing everything")
+#    print(response)
+#    print("printing one thing")
+ #   item_name = response['Contents'][0]
+    for i in response['Contents']:
+        for k, v in i.items():
+            if k == 'Key':
+                print(v)
 
 
